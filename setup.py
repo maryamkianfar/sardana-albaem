93eb278dbcf97da9577ef4bf7310f82cba7a477f
Carlos Falcon <cfalcon@cells.es>
2021-09-28 16:48:17 +0200

    Bump version 0.0.10 to 0.0.11

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def main():
     """Main method collecting all the parameters to setup."""
     name = "sardana-albaem"
 
-    version = "0.0.10"
+    version = "0.0.11"
 
     description = "AlbaEM Sardana Controller"
 
